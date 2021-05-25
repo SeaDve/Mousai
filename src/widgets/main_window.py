814d85ec2fd1cde9e37cf9ef8c96e39a7f10157a
SeaDve <davecruz48@gmail.com>
2021-05-25 12:18:51 +0800

    fix: Angry CI

--- src/widgets/main_window.py
+++ src/widgets/main_window.py
@@ -15,8 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from collections import namedtuple
-
 from gi.repository import GLib, Gtk, Adw, Gio
 
 from mousai.widgets.song import Song
