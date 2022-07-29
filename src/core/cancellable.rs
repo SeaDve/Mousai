af9b252519bac7d4f5791b0618e65c3e0ff54ae3
SeaDve <davecruz48@gmail.com>
2022-07-29 21:38:25 +0800

    chore(cancellable): Add tests

--- src/core/cancellable.rs
+++ src/core/cancellable.rs
@@ -23,6 +23,7 @@ impl std::error::Error for Cancelled {}
 
 type CancelledCallback = Box<dyn FnOnce(&Cancellable) + 'static>;
 
+/// Single threaded cancellable.
 #[derive(Default)]
 pub struct Cancellable {
     callbacks: RefCell<Vec<CancelledCallback>>,
@@ -38,6 +39,8 @@ impl std::fmt::Debug for Cancellable {
 }
 
 impl Cancellable {
+    /// Cancel and trigger all the callbacks. Cancelling again
+    /// is a no-op.
     pub fn cancel(&self) {
         if self.is_cancelled() {
             log::warn!("Trying to cancel a cancelled cancellable");
@@ -51,11 +54,91 @@ impl Cancellable {
         }
     }
 
+    /// Returns true if the cancellable has been cancelled.
     pub fn is_cancelled(&self) -> bool {
         self.is_cancelled.get()
     }
 
+    /// Register a callback to be called when the cancellable is cancelled.
+    ///
+    /// This would be called only once, even if the cancellable is cancelled
+    /// again.
     pub fn connect_cancelled(&self, callback: impl FnOnce(&Self) + 'static) {
         self.callbacks.borrow_mut().push(Box::new(callback));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::rc::Rc;
+
+    #[test]
+    fn cancel() {
+        let c = Cancellable::default();
+        assert!(!c.is_cancelled());
+
+        c.cancel();
+        assert!(c.is_cancelled());
+    }
+
+    #[test]
+    fn cancel_twice() {
+        let c = Cancellable::default();
+        assert!(!c.is_cancelled());
+
+        let n_called = Rc::new(Cell::new(0));
+
+        let n_called_clone = Rc::clone(&n_called);
+        c.connect_cancelled(move |c| {
+            assert!(c.is_cancelled());
+            n_called_clone.set(n_called_clone.get() + 1);
+        });
+        assert_eq!(n_called.get(), 0);
+
+        c.cancel();
+        assert_eq!(n_called.get(), 1);
+
+        c.cancel();
+        assert_eq!(n_called.get(), 1);
+    }
+
+    #[test]
+    fn connect() {
+        let c = Cancellable::default();
+        assert!(!c.is_cancelled());
+
+        let called = Rc::new(Cell::new(false));
+
+        let called_clone = Rc::clone(&called);
+        c.connect_cancelled(move |c| {
+            assert!(c.is_cancelled());
+            called_clone.set(true);
+        });
+        assert!(!called.get());
+
+        c.cancel();
+        assert!(called.get());
+    }
+
+    #[test]
+    fn connect_after_cancelled() {
+        let c = Cancellable::default();
+        assert!(!c.is_cancelled());
+
+        c.cancel();
+        assert!(c.is_cancelled());
+
+        let called = Rc::new(Cell::new(false));
+
+        let called_clone = Rc::clone(&called);
+        c.connect_cancelled(move |c| {
+            assert!(c.is_cancelled());
+            called_clone.set(true);
+        });
+        assert!(called.get());
+
+        assert!(c.is_cancelled());
     }
 }
