c8b2577db0702546c5b5e3fe94475351a26d6f7e
SeaDve <davecruz48@gmail.com>
2022-12-26 09:08:43 +0800

    fix: use colon instead of period in log for consistency

--- src/audio_device.rs
+++ src/audio_device.rs
@@ -86,7 +86,7 @@ fn find_default_name_gst(class: AudioDeviceClass) -> Result<String> {
             Ok(is_default) => is_default,
             Err(err) => {
                 tracing::warn!(
-                    "Skipping device `{}` as it has no `is-default` property. {:?}",
+                    "Skipping device `{}` as it has no `is-default` property: {:?}",
                     device.name(),
                     err
                 );
