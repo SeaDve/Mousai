3d6f4bc52408f7273bcbbcf32460251f8d909ccd
SeaDve <davecruz48@gmail.com>
2023-03-01 10:41:58 +0800

    feat: implement migration from memory-list from pre v0.6.6

--- src/settings.rs
+++ src/settings.rs
@@ -1,9 +1,16 @@
 use gsettings_macro::gen_settings;
 use gtk::gio;
 
+use std::collections::HashMap;
+
 use crate::config::APP_ID;
 
 #[gen_settings(file = "./data/io.github.seadve.Mousai.gschema.xml.in")]
+#[gen_settings_define(
+    key_name = "memory-list",
+    arg_type = "Vec<HashMap<String, String>>",
+    ret_type = "Vec<HashMap<String, String>>"
+)]
 pub struct Settings;
 
 impl Default for Settings {
