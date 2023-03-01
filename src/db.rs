c20f4e912927e949aee3a1bf5f88dca18af2eb25
SeaDve <davecruz48@gmail.com>
2023-03-01 10:57:26 +0800

    refactor: rename N_DB to N_NAMED_DB

--- src/db.rs
+++ src/db.rs
@@ -3,14 +3,13 @@ use gtk::glib;
 
 use std::fs;
 
+const N_NAMED_DB: u32 = 2;
 pub const SONG_LIST_DB_NAME: &str = "song_list";
 pub const RECORDINGS_DB_NAME: &str = "saved_recordings";
-
-const N_DB: u32 = 2;
 
 pub fn init_env() -> Result<heed::Env> {
     let path = glib::user_data_dir().join("mousai/db");
     fs::create_dir_all(&path)?;
-    let env = heed::EnvOpenOptions::new().max_dbs(N_DB).open(path)?;
+    let env = heed::EnvOpenOptions::new().max_dbs(N_NAMED_DB).open(path)?;
     Ok(env)
 }
