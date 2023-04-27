8cc352e458802a59882b717529efb295bd696718
SeaDve <davecruz48@gmail.com>
2023-04-27 20:17:31 +0800

    refactor: use future mutex as a lock instead of manual impl
    It is much less prone to bugs

--- src/core/album_art_store/album_art.rs
+++ src/core/album_art_store/album_art.rs
@@ -1,11 +1,10 @@
-use anyhow::Result;
-use futures_channel::oneshot::{self, Receiver, Sender};
-use futures_util::future::{FutureExt, Shared};
+use anyhow::{Context, Result};
+use futures_util::lock::Mutex;
 use gtk::{gdk, gio, glib, prelude::*};
 use once_cell::unsync::OnceCell;
 use soup::prelude::*;
 
-use std::{cell::RefCell, fmt, path::Path};
+use std::path::Path;
 
 // TODO
 // - Don't load AlbumArt if network is metered
@@ -18,27 +17,9 @@ pub struct AlbumArt {
     session: soup::Session,
     download_url: String,
     cache_file: gio::File,
+
     cache: OnceCell<gdk::Texture>,
-    #[allow(clippy::type_complexity)]
-    guard: RefCell<Option<(Sender<()>, Shared<Receiver<()>>)>>,
-}
-
-impl fmt::Debug for AlbumArt {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let rx_strong_count = self
-            .guard
-            .borrow()
-            .as_ref()
-            .and_then(|(_, rx)| rx.strong_count())
-            .unwrap_or(0);
-
-        f.debug_struct("AlbumArt")
-            .field("download_url", &self.download_url)
-            .field("cache_uri", &self.cache_file.uri())
-            .field("loaded", &self.is_loaded())
-            .field("rx_strong_count", &rx_strong_count)
-            .finish()
-    }
+    cache_guard: Mutex<()>,
 }
 
 impl AlbumArt {
@@ -48,7 +29,7 @@ impl AlbumArt {
             download_url: download_url.to_string(),
             cache_file: gio::File::for_path(cache_path),
             cache: OnceCell::new(),
-            guard: RefCell::default(),
+            cache_guard: Mutex::new(()),
         }
     }
 
@@ -66,41 +47,27 @@ impl AlbumArt {
     }
 
     pub async fn texture(&self) -> Result<&gdk::Texture> {
-        let rx = self.guard.borrow().as_ref().map(|(_, rx)| rx.clone());
-        if let Some(rx) = rx {
-            // If there are currently loading AlbumArt, wait
-            // for it to finish and be stored before checking if
-            // it exist. This is to prevent loading the same
-            // album art twice on subsequent call on this function.
-            let _ = rx.await;
-        }
-
-        debug_assert!(
-            self.guard.borrow().is_none(),
-            "guard must be dropped before checking cache"
-        );
+        let guard = self.cache_guard.lock().await;
 
         if let Some(texture) = self.cache.get() {
             return Ok(texture);
         }
 
-        let (tx, rx) = oneshot::channel();
-        self.guard.replace(Some((tx, rx.shared())));
-
         match self.cache_file.load_bytes_future().await {
             Ok((ref bytes, _)) => {
-                let texture = gdk::Texture::from_bytes(bytes)?;
+                let texture = gdk::Texture::from_bytes(bytes)
+                    .context("Failed to load album art texture from bytes")?;
                 return Ok(self.set_and_get_cache(texture));
             }
             Err(err) => {
-                if err.matches(gio::IOErrorEnum::NotFound) {
-                    tracing::debug!(
-                        uri = ?self.cache_file.uri(),
-                        "Cache file not found; downloading album art",
-                    );
-                } else {
-                    return Err(err.into());
+                if !err.matches(gio::IOErrorEnum::NotFound) {
+                    return Err(err).context("Failed to load album art bytes from cache")?;
                 }
+
+                tracing::debug!(
+                    uri = ?self.cache_file.uri(),
+                    "Cache file not found; downloading album art",
+                );
             }
         }
 
@@ -110,38 +77,39 @@ impl AlbumArt {
                 &soup::Message::new("GET", &self.download_url)?,
                 glib::PRIORITY_LOW,
             )
-            .await?;
-        tracing::debug!(download_url = ?self.download_url, "Downloaded album art");
-
-        let texture = self.set_and_get_cache(gdk::Texture::from_bytes(&bytes)?);
-
-        let texture_bytes = texture.save_to_png_bytes();
-        self.cache_file
-            .replace_contents_future(texture_bytes, None, false, gio::FileCreateFlags::NONE)
             .await
-            .map_err(|(_, err)| err)?;
+            .context("Failed to download album art bytes")?;
+        tracing::debug!(download_url = ?self.download_url, "Downloaded album art bytes");
+
+        let texture = gdk::Texture::from_bytes(&bytes)
+            .context("Failed to load album art texture from bytes")?;
+        let texture = self.set_and_get_cache(texture);
+
+        // We don't need to hold the lock anymore since this function would
+        // early return even if we are still saving the texture to disk.
+        drop(guard);
+
+        let png_bytes = texture.save_to_png_bytes();
+        self.cache_file
+            .replace_contents_future(png_bytes, None, false, gio::FileCreateFlags::NONE)
+            .await
+            .map_err(|(_, err)| err)
+            .context("Failed to save album art texture to cache file")?;
+        tracing::debug!(uri = ?self.cache_file.uri(), "Saved album art to cache file");
 
         Ok(texture)
     }
 
     fn set_and_get_cache(&self, texture: gdk::Texture) -> &gdk::Texture {
-        let ret = match self.cache.try_insert(texture) {
+        match self.cache.try_insert(texture) {
             Ok(final_value) => final_value,
             Err((final_value, texture)) => {
-                tracing::error!(
-                    "cache was already set; is_same_instance = {}",
+                unreachable!(
+                    "cache must not already be set; is_same_instance = {}",
                     final_value == &texture,
                 );
-                final_value
             }
-        };
-
-        // Since the cache is already loaded, the guard to
-        // delay consecutive calls to Self::texture is not
-        // needed anymore.
-        self.guard.replace(None);
-
-        ret
+        }
     }
 
     #[cfg(test)]
