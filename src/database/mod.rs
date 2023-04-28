fcbfd37fe40bd18dacbf8fe8ab40b090a15e9a65
SeaDve <davecruz48@gmail.com>
2023-04-28 20:35:30 +0800

    misc!: drop WriteMap and MapAsync lmdb env flags
    This makes the db more robust

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -20,15 +20,11 @@ pub const RECORDINGS_DB_NAME: &str = "saved_recordings";
 pub fn new_env() -> Result<heed::Env> {
     let path = glib::user_data_dir().join("mousai/db");
     fs::create_dir_all(&path)?;
-    let env = unsafe {
-        heed::EnvOpenOptions::new()
-            .map_size(100 * 1024 * 1024) // 100 MiB
-            .max_dbs(N_NAMED_DBS)
-            .flag(heed::Flags::MdbWriteMap)
-            .flag(heed::Flags::MdbMapAsync)
-            .open(&path)
-            .with_context(|| format!("Failed to open heed env at {}", path.display()))?
-    };
+    let env = heed::EnvOpenOptions::new()
+        .map_size(100 * 1024 * 1024) // 100 MiB
+        .max_dbs(N_NAMED_DBS)
+        .open(&path)
+        .with_context(|| format!("Failed to open heed env at {}", path.display()))?;
 
     tracing::debug!(
         ?path,
@@ -45,15 +41,11 @@ pub fn new_env() -> Result<heed::Env> {
 #[cfg(test)]
 pub fn new_test_env() -> (heed::Env, tempfile::TempDir) {
     let tempdir = tempfile::tempdir().unwrap();
-    let env = unsafe {
-        heed::EnvOpenOptions::new()
-            .map_size(100 * 1024 * 1024) // 100 MiB
-            .max_dbs(1)
-            .flag(heed::Flags::MdbWriteMap)
-            .flag(heed::Flags::MdbMapAsync)
-            .open(&tempdir)
-            .unwrap()
-    };
+    let env = heed::EnvOpenOptions::new()
+        .map_size(100 * 1024 * 1024) // 100 MiB
+        .max_dbs(1)
+        .open(&tempdir)
+        .unwrap();
     (env, tempdir)
 }
 
