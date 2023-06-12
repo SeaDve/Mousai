d3b9886b70f0d7ff82ea486d2b51749b8f5fa75a
SeaDve <davecruz48@gmail.com>
2023-06-12 21:16:55 +0800

    refactor: make PlaybackButton a subclass of GtkButton
    It fixes the incorrect styling on osd hover.
    It also simplify things a bit.

--- src/window/playback_button.rs
+++ src/window/playback_button.rs
@@ -14,7 +14,6 @@ pub enum PlaybackButtonMode {
 
 mod imp {
     use super::*;
-    use std::marker::PhantomData;
 
     #[derive(Default, glib::Properties, gtk::CompositeTemplate)]
     #[properties(wrapper_type = super::PlaybackButton)]
@@ -23,12 +22,7 @@ mod imp {
         /// State or mode
         #[property(get, set = Self::set_mode, explicit_notify, builder(PlaybackButtonMode::default()))]
         pub(super) mode: Cell<PlaybackButtonMode>,
-        /// Name of the action to trigger when clicked
-        #[property(get = Self::action_name, set = Self::set_action_name, explicit_notify)]
-        pub(super) action_name: PhantomData<Option<glib::GString>>,
 
-        #[template_child]
-        pub(super) button: TemplateChild<gtk::Button>,
         #[template_child]
         pub(super) stack: TemplateChild<gtk::Stack>,
         #[template_child]
@@ -41,13 +35,10 @@ mod imp {
     impl ObjectSubclass for PlaybackButton {
         const NAME: &'static str = "MsaiPlaybackButton";
         type Type = super::PlaybackButton;
-        type ParentType = gtk::Widget;
+        type ParentType = gtk::Button;
 
         fn class_init(klass: &mut Self::Class) {
             klass.bind_template();
-
-            klass.set_accessible_role(gtk::AccessibleRole::Button);
-            klass.set_css_name("playbackbutton");
         }
 
         fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
@@ -65,13 +56,10 @@ mod imp {
 
             obj.update_ui();
         }
-
-        fn dispose(&self) {
-            self.dispose_template();
-        }
     }
 
     impl WidgetImpl for PlaybackButton {}
+    impl ButtonImpl for PlaybackButton {}
 
     impl PlaybackButton {
         fn set_mode(&self, mode: PlaybackButtonMode) {
@@ -85,21 +73,12 @@ mod imp {
             obj.update_ui();
             obj.notify_mode();
         }
-
-        fn action_name(&self) -> Option<glib::GString> {
-            self.button.action_name()
-        }
-
-        fn set_action_name(&self, action_name: Option<&str>) {
-            self.button.set_action_name(action_name);
-            self.obj().notify_action_name();
-        }
     }
 }
 
 glib::wrapper! {
     pub struct PlaybackButton(ObjectSubclass<imp::PlaybackButton>)
-        @extends gtk::Widget;
+        @extends gtk::Widget, gtk::Button;
 }
 
 impl PlaybackButton {
@@ -114,19 +93,19 @@ impl PlaybackButton {
             PlaybackButtonMode::Play => {
                 imp.image_child
                     .set_icon_name(Some("media-playback-start-symbolic"));
-                imp.button.set_tooltip_text(Some(&gettext("Play")));
+                self.set_tooltip_text(Some(&gettext("Play")));
                 imp.stack.set_visible_child(&imp.image_child.get());
                 imp.spinner_child.set_spinning(false);
             }
             PlaybackButtonMode::Pause => {
                 imp.image_child
                     .set_icon_name(Some("media-playback-pause-symbolic"));
-                imp.button.set_tooltip_text(Some(&gettext("Pause")));
+                self.set_tooltip_text(Some(&gettext("Pause")));
                 imp.stack.set_visible_child(&imp.image_child.get());
                 imp.spinner_child.set_spinning(false);
             }
             PlaybackButtonMode::Buffering => {
-                imp.button.set_tooltip_text(None);
+                self.set_tooltip_text(None);
                 imp.stack.set_visible_child(&imp.spinner_child.get());
                 imp.spinner_child.set_spinning(true);
             }
