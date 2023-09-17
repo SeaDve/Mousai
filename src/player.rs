a0822021f78013fffc2a83826d76f044bcfb2f84
Dave Patrick Caberto <davecruz48@gmail.com>
2023-09-17 16:27:14 +0800

    misc: bump mpris-server

--- src/player.rs
+++ src/player.rs
@@ -101,16 +101,12 @@ mod imp {
                 Ok(server) => {
                     self.mpris_server.set(server).unwrap();
 
-                    utils::spawn(
-                        glib::Priority::default(),
-                        clone!(@weak obj => async move {
-                            if let Err(err) =
-                                obj.imp().mpris_server.get().unwrap().init_and_run().await
-                            {
-                                tracing::error!("Failed to run MPRIS server: {:?}", err);
-                            }
-                        }),
-                    );
+                    let task = self.mpris_server.get().unwrap().init_and_run();
+                    utils::spawn(glib::Priority::default(), async move {
+                        if let Err(err) = task.await {
+                            tracing::error!("Failed to run MPRIS server: {:?}", err);
+                        }
+                    });
                 }
                 Err(err) => {
                     tracing::error!("Failed to create MPRIS server: {:?}", err);
