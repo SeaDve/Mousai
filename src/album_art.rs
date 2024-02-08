a20efbcdafad934853d489ef12f9ddd8a4666418
Dave Patrick Caberto <davecruz48@gmail.com>
2024-02-08 19:57:59 +0800

    refactor: use async OnceCell

--- src/album_art.rs
+++ src/album_art.rs
@@ -1,11 +1,7 @@
-use std::{
-    cell::{OnceCell, RefCell},
-    collections::HashMap,
-    rc::Rc,
-};
+use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
 use anyhow::{Context, Result};
-use futures_util::lock::Mutex;
+use async_lock::OnceCell;
 use gtk::{gdk, glib};
 use soup::prelude::*;
 
@@ -44,9 +40,7 @@ impl AlbumArtStore {
 pub struct AlbumArt {
     session: soup::Session,
     download_url: String,
-
     cache: OnceCell<gdk::Texture>,
-    cache_guard: Mutex<()>,
 }
 
 impl AlbumArt {
@@ -55,13 +49,12 @@ impl AlbumArt {
             session,
             download_url: download_url.to_string(),
             cache: OnceCell::new(),
-            cache_guard: Mutex::new(()),
         }
     }
 
     /// Whether the album art is loaded in memory.
     pub fn is_loaded(&self) -> bool {
-        self.cache.get().is_some()
+        self.cache.is_initialized()
     }
 
     pub fn download_url(&self) -> &str {
@@ -69,27 +62,23 @@ impl AlbumArt {
     }
 
     pub async fn texture(&self) -> Result<&gdk::Texture> {
-        let _guard = self.cache_guard.lock().await;
+        self.cache
+            .get_or_try_init(|| async {
+                let bytes = self
+                    .session
+                    .send_and_read_future(
+                        &soup::Message::new("GET", &self.download_url)?,
+                        glib::Priority::LOW,
+                    )
+                    .await
+                    .context("Failed to download album art bytes")?;
+                tracing::trace!(download_url = ?self.download_url, "Downloaded album art bytes");
 
-        if let Some(texture) = self.cache.get() {
-            return Ok(texture);
-        }
-
-        let bytes = self
-            .session
-            .send_and_read_future(
-                &soup::Message::new("GET", &self.download_url)?,
-                glib::Priority::LOW,
-            )
+                let texture = gdk::Texture::from_bytes(&bytes)
+                    .context("Failed to load album art texture from bytes")?;
+                Ok(texture)
+            })
             .await
-            .context("Failed to download album art bytes")?;
-        tracing::trace!(download_url = ?self.download_url, "Downloaded album art bytes");
-
-        let texture = gdk::Texture::from_bytes(&bytes)
-            .context("Failed to load album art texture from bytes")?;
-        self.cache.set(texture).unwrap();
-
-        Ok(self.cache.get().unwrap())
     }
 }
 
