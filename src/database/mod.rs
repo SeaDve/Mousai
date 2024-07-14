b288413ad697a3b537a323e352259eeeeecf7775
Dave Patrick Caberto <davecruz48@gmail.com>
2024-07-14 11:18:35 +0800

    fix: use unsafe block in opening env

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -21,11 +21,13 @@ pub fn new_env() -> Result<heed::Env> {
     let path = glib::user_data_dir().join("mousai/db");
     fs::create_dir_all(&path)
         .with_context(|| format!("Failed to create db dir at {}", path.display()))?;
-    let env = heed::EnvOpenOptions::new()
-        .map_size(100 * 1024 * 1024) // 100 MiB
-        .max_dbs(N_NAMED_DBS)
-        .open(&path)
-        .with_context(|| format!("Failed to open heed env at {}", path.display()))?;
+    let env = unsafe {
+        heed::EnvOpenOptions::new()
+            .map_size(100 * 1024 * 1024) // 100 MiB
+            .max_dbs(N_NAMED_DBS)
+            .open(&path)
+            .with_context(|| format!("Failed to open heed env at {}", path.display()))?
+    };
 
     tracing::debug!(
         ?path,
@@ -43,11 +45,13 @@ pub fn new_env() -> Result<heed::Env> {
 #[cfg(test)]
 pub fn new_test_env() -> (heed::Env, tempfile::TempDir) {
     let tempdir = tempfile::tempdir().unwrap();
-    let env = heed::EnvOpenOptions::new()
-        .map_size(100 * 1024 * 1024) // 100 MiB
-        .max_dbs(1)
-        .open(&tempdir)
-        .unwrap();
+    let env = unsafe {
+        heed::EnvOpenOptions::new()
+            .map_size(100 * 1024 * 1024) // 100 MiB
+            .max_dbs(1)
+            .open(&tempdir)
+            .unwrap()
+    };
     (env, tempdir)
 }
 
